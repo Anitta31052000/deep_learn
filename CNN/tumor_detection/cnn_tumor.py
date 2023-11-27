d0a0d8ab1c27d8dc942ae22a6aadb4af6fbfe307
Anitta31052000 <125797144+Anitta31052000@users.noreply.github.com>
2023-11-27 22:51:05 +0530

    Update cnn_tumor.py

--- CNN/tumor_detection/cnn_tumor.py
+++ CNN/tumor_detection/cnn_tumor.py
@@ -6,10 +6,12 @@ import tensorflow as tf
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from tqdm import tqdm
-import pickle
 
-image_dir=r'D:\S3\stream_model\CNN\tumordata'
+
+
+from tqdm import tqdm
+
+image_dir=r"D:\deephub\Deep-Prediction-Hub\CNN\tumordata"
 no_tumor_images=os.listdir(image_dir+ '/no')
 yes_tumor_images=os.listdir(image_dir+ '/yes')
 print("--------------------------------------\n")
@@ -59,11 +61,12 @@ print("--------------------------------------\n")
 print("--------------------------------------\n")
 print("Normalaising the Dataset. \n")
 
-# x_train=x_train.astype('float')/255
-# x_test=x_test.astype('float')/255 
+x_train=x_train.astype('float')/255
+
+x_test=x_test.astype('float')/255 
 
 # Same step above is implemented using tensorflow functions.
-
+print("....... ......")
 x_train=tf.keras.utils.normalize(x_train,axis=1)
 x_test=tf.keras.utils.normalize(x_test,axis=1)
 
@@ -75,7 +78,7 @@ model=tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D((2,2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(256,activation='relu'),
-    tf.keras.layers.Dropout(.5),
+    tf.keras.layers.Dropout(0.5),
     tf.keras.layers.Dense(512,activation='relu'),
     tf.keras.layers.Dense(1,activation='sigmoid')
 ])
@@ -90,45 +93,25 @@ model.compile(optimizer='adam',
 
 print("--------------------------------------\n")
 print("Training Started.\n")
-history=model.fit(x_train,y_train,epochs=5,batch_size =128,validation_split=0.1)
+history=model.fit(x_train,y_train,epochs=20,batch_size =128,validation_split=0.1)
 print("Training Finished.\n")
 print("--------------------------------------\n")
 
 
-# Plot and save accuracy
-plt.plot(history.epoch,history.history['accuracy'], label='accuracy')
-plt.plot(history.epoch,history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.ylim([0, 1])
-plt.legend(loc='lower right')
-plt.savefig(r'D:\S3\stream_model\CNN\tumor_detection\results\tumor_sample_accuracy_plot.png')
-
-# Clear the previous plot
-plt.clf()
-
-# Plot and save loss
-plt.plot(history.epoch,history.history['loss'], label='loss')
-plt.plot(history.epoch,history.history['val_loss'], label = 'val_loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.legend(loc='upper right')
-plt.savefig(r'D:\S3\stream_model\CNN\tumor_detection\results\tumor_sample_loss_plot.png')
-
 
 print("--------------------------------------\n")
+
 print("Model Evalutaion Phase.\n")
 loss,accuracy=model.evaluate(x_test,y_test)
 print(f'Accuracy: {round(accuracy*100,2)}')
 print("--------------------------------------\n")
+
+
 y_pred=model.predict(x_test)
 y_pred = (y_pred > 0.5).astype(int)
 print('classification Report\n',classification_report(y_test,y_pred))
 print("--------------------------------------\n")
 
-
-print("--------------------------------------\n")
-print("Model Prediction.\n")
 
 def make_prediction(img,model):
     img=cv2.imread(img)
@@ -141,13 +124,5 @@ def make_prediction(img,model):
         print("Tumor Detected")
     else:
         print("No Tumor")
-        
-make_prediction(r'D:\S3\stream_model\CNN\tumordata\no\no5.jpg',model)
-print("--------------------------------------\n")
-make_prediction(r'D:\S3\stream_model\CNN\tumordata\yes\y4.jpg',model)
-print("--------------------------------------\n")
 
-#To save model
-filename = r'D:\S3\stream_model\CNN\saved_cnn.pkl'
-pickle.dump(model, open(filename,'wb'))
-
+model.save(r'D:\deephub\CN.keras')
