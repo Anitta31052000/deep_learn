46139dfdb9f5ce22a7081b7c4bee90f7ae3809bc
Anitta31052000 <125797144+Anitta31052000@users.noreply.github.com>
2024-01-10 14:26:42 +0530

    Update smsspam.py

--- RNN/smsspam.py
+++ RNN/smsspam.py
@@ -3,26 +3,46 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
-
-
-from numpy import argmax
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.optimizers import RMSprop, Adam
-from tensorflow.keras.datasets import imdb
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 import pickle
 
-top_words = 5000
-(X_train, y_train), (X_test,y_test) = imdb.load_data(num_words=top_words)
+print("---------------------- Downloading Dataset -------------------------\n")
 
-max_review_length = 500
-X_train = pad_sequences(X_train, maxlen=max_review_length)
-X_test = pad_sequences(X_test, maxlen=max_review_length)
+dataset = pd.read_csv(r"C:\Users\hp\Downloads\SMSSpamCollection.txt",sep='\t',names=['label','message'])
+
+print("----------------------  -------------------------\n")
+print(dataset.head())
+print("----------------------  -------------------------")
+print(dataset.groupby('label').describe())
+print("----------------------  -------------------------")
+dataset['label'] = dataset['label'].map( {'spam': 1, 'ham': 0} )
+X = dataset['message'].values
+y = dataset['label'].values
+
+print("---------------------- Train Test Split -------------------------\n")
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+tokeniser = tf.keras.preprocessing.text.Tokenizer()
+tokeniser.fit_on_texts(X_train)
+encoded_train = tokeniser.texts_to_sequences(X_train)
+encoded_test = tokeniser.texts_to_sequences(X_test)
+print(encoded_train[0:2])
+print("----------------------  Padding  -------------------------\n")
+max_length = 10
+padded_train = tf.keras.preprocessing.sequence.pad_sequences(encoded_train, maxlen=max_length, padding='post')
+padded_test = tf.keras.preprocessing.sequence.pad_sequences(encoded_test, maxlen=max_length, padding='post')
+print(padded_train[0:2])
+
+print("----------------------  -------------------------\n")
+
+vocab_size = len(tokeniser.word_index)+1
+
+# define the model
+
+print("---------------------- Modelling -------------------------\n")
+
 
 model=tf.keras.models.Sequential([
-   tf.keras.layers.Embedding(input_dim=top_words,output_dim= 24, input_length=max_review_length),
+   tf.keras.layers.Embedding(input_dim=vocab_size,output_dim= 24, input_length=max_length),
    tf.keras.layers.SimpleRNN(24, return_sequences=False),
    tf.keras.layers.Dense(64, activation='relu'),
    tf.keras.layers.Dense(32, activation='relu'),
@@ -44,22 +64,34 @@ early_stop = tf.keras.callbacks.EarlyStopping(monitor='accuracy', mode='min', pa
 print("----------------------  Training -------------------------\n")
 
 # fit the model
-model.fit(x=X_train,
+model.fit(x=padded_train,
          y=y_train,
-         epochs=100,
-         validation_data=(X_test, y_test),
+         epochs=50,
+         validation_data=(padded_test, y_test),
          callbacks=[early_stop]
          )
 print("----------------------  -------------------------\n")
 
 
-def acc_report(y_true, y_pred):
+def c_report(y_true, y_pred):
+   print("Classification Report")
+   print(classification_report(y_true, y_pred))
    acc_sc = accuracy_score(y_true, y_pred)
    print(f"Accuracy : {str(round(acc_sc,2)*100)}")
    return acc_sc
 
+def plot_confusion_matrix(y_true, y_pred):
+   mtx = confusion_matrix(y_true, y_pred)
+   sns.heatmap(mtx, annot=True, fmt='d', linewidths=.5, cmap="Blues", cbar=False)
+   plt.ylabel('True label')
+   plt.xlabel('Predicted label')
+   plt.savefig(r'D:\S3\stream_model\RNN\results\test.jpg')
 
-preds = (model.predict(X_test) > 0.5).astype("int32")
-print(acc_report(y_test, preds))
 
-model.save(r'D:\deephub\Deep-Prediction-Hub\RN.keras')
+preds = (model.predict(padded_test) > 0.5).astype("int32")
+c_report(y_test, preds)
+plot_confusion_matrix(y_test, preds)
+
+#To save model
+filename = r'D:\S3\stream_model\CNN\saved_rnn.pkl'
+pickle.dump(model, open(filename,'wb'))
